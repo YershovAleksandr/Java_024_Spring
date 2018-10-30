c61864d931477ec207b9dfab582495c9c6f14f93
hp <namstudionsk@gmail.com>
2018-10-30 14:49:26 +0700

    Fix

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController{
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private Date date = new Date();
 
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="Worlds") String name){
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		//date = new Date()
+		return new Greeting(counter.incrementAndGet(), String.format(template, name), date);
 	}
 }
